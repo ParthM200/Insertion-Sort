e51f04205d9807fbe0bd84cfd15ce29702a0c37c
Parth Mehta <parthpm06@gmail.com>
2024-08-05 10:16:01 -0400

    update

--- insertionSort.java
+++ insertionSort.java
@@ -1,34 +1,28 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class insertionSort {
+
     private ArrayList<Integer> myIntAL = new ArrayList<Integer>();
-    private int numToSort = 0;
+    int numToSort = 0;
 
+    Scanner scanner = new Scanner(System.in);
+
+    // Constructor
     public insertionSort() {
-        System.out.println("************  Unsorted **********************");
-        numToSort = 10;// Number of items to create and sort
-        buildArray(); // Creates a random ArrayList
-        printArray(); // Prints the unsorted arraylist
-        System.out.println("***********************************************");
-
-        insertionSort(myIntAL); // Performs the bubble sort on the arraylist
-        System.out.println("***********  Sorted   ******************");
-        printArray();// Prints the sorted arraylist
-        System.out.println("***********************************************");
+        runUserInput();
     }
 
-    // Builds array of random numbers to sort
+    // Builds array based on user input
     public void buildArray() {
         for (int i = 0; i < numToSort; i++) {
-            myIntAL.add((int) (Math.random() * numToSort));
+            System.out.print("Enter number " + (i + 1) + ": ");
+            int userInt = scanner.nextInt();
+            myIntAL.add(userInt);
         }
     }
 
-    public void printArray() {
-        System.out.println(myIntAL);
-    }
-
-    // Sorts the array
+    // Insertion sort method
     public void insertionSort(ArrayList<Integer> arr) {
         int n = arr.size();
         for (int i = 1; i < n; i++) {
@@ -40,9 +34,26 @@ public class insertionSort {
             }
             arr.set(j + 1, key);
 
-            System.out.print("Cycle " + i + " ");
-            printArray();
+            System.out.print("Insertion Sort Cycle " + i + ": ");
+            System.out.println(myIntAL);
         }
+    }
+
+    // Asks user for their input and runs all the processes to build and sort the
+    // arraylist
+    public void runUserInput() {
+        System.out.print("\nHow long do you want the ArrayList to be?: ");
+        numToSort = scanner.nextInt();
+
+        buildArray();
+        System.out.println("\n***********  Unsorted ArrayList   ******************");
+        System.out.println(myIntAL);
+        System.out.println();
+
+        insertionSort(myIntAL);
+        System.out.println("\n***********  Final Sorted ArrayList  ******************");
+        System.out.println(myIntAL);
+        System.out.println();
     }
 
 }
